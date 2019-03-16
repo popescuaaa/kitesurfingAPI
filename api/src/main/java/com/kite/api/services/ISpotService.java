b49f0beebe650ca24acda202d9fb18ee77c9d2f0
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2019-03-16 18:44:43 +0200

    final version - document about bycrypt

--- api/src/main/java/com/kite/api/services/ISpotService.java
+++
@@ -1,9 +0,0 @@
-package com.kite.api.services;
-
-import com.kite.api.entities.*;
-import java.util.List;
-
-public interface ISpotService {
-    List<Spot> findAll();
-    List<String> findAllCountries();
-}
